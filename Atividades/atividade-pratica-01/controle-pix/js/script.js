85a99aa2aefef97e12e5493b95a450e4243f95ad
gust4avofonseca <gustavofonseca27@gmail.com>
2022-05-19 22:17:34 -0300

    carregar

--- Atividades/atividade-pratica-01/controle-pix/js/script.js
+++ Atividades/atividade-pratica-01/controle-pix/js/script.js
@@ -2,6 +2,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
     loadBanks();
 })
 
+const transactions = [];
+
+const key = document.getElementById("key");
+const value = document.getElementById("value");
+const even = document.getElementById("event");
+const bank = document.getElementById("bank");
+const transactionsDiv = document.getElementById("transactions"); 
+
 function loadBanks() {
     fetch("https://brasilapi.com.br/api/banks/v1")
         .then(response => response.json())
@@ -15,4 +23,20 @@ function loadBanks() {
             }
            
         })
+}
+
+function create() {
+    const data = {
+        key: key.value, 
+        value: value.value, 
+        even: even.value, 
+        bank: bank.value
+    }
+    transactions.push(data);
+}
+
+function viewtransactions() {
+    for (const transaction of transactions) {
+        
+    }
 }
